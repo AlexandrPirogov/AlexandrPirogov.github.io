16bd66452db31f3066263e5f8c1b040e82f2242b
AlexandrPirogov <johndoe@example.com>
2021-04-25 11:38:17 -0800

    Here we go

--- Scripts/CreatePost.php
+++ Scripts/CreatePost.php
@@ -1,4 +1,2 @@
 <?php
     print(json_encode($_POST));
-    sleep(1);
-    header("Location: feed");
--- View/FeedView.php
+++ View/FeedView.php
@@ -49,3 +49,9 @@
             </div>
         </div>
     </div>";
+require_once './Scripts/db_config.php';
+$db_connection = pg_connect("host=localhost port=8090 user=$username password=$password");
+$res = pg_query($db_connection, "select * from pirogovblog.users u ;");
+while($row = pg_fetch_row($res)){
+    echo $row[1];
+}
